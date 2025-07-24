b40ab1181ea9d11481a1fe2aa9753518d1b8564c
danieln3m0 <u202219315@upc.edu.pe>
2025-07-24 16:45:44 -0500

    feat: Enhance security configuration with JWT authentication filter

--- src/main/java/com/las4as/POSBackend/IAM/Infrastructure/authorization/sfs/configuration/SecurityConfiguration.java
+++ src/main/java/com/las4as/POSBackend/IAM/Infrastructure/authorization/sfs/configuration/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.las4as.POSBackend.IAM.Infrastructure.authorization.sfs.configuration;
 
+import com.las4as.POSBackend.IAM.Infrastructure.authorization.sfs.filters.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -14,7 +17,10 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -35,7 +41,9 @@ public class SecurityConfiguration {
                 .requestMatchers("/api/v1/users").permitAll()
                 // Requerir autenticación para el resto
                 .anyRequest().authenticated()
-            );
+            )
+            // Agregar el filtro JWT antes del filtro de autenticación de usuario/contraseña
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         
         return http.build();
     }
